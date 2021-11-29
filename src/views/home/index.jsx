a10c063d517f51138e041f96c9a7a04a02963bca
wxw <359443034@qq.com>
2021-11-29 17:39:36 +0800

    账单页面开发

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon, Pull } from "zarm";
 
 import s from "./index.module.less";
 
 export default () => {
   document.title = "账单";
-
+  const [billList, setBillList] = useState([1, 1, 1, 1]);
   return (
     <>
       <div className={s.wrapper}>
@@ -30,29 +30,33 @@ export default () => {
         </div>
 
         <div className={s.list}>
-          <div className={s.item}>
-            <div className={s.itemTop}>
-              <div className={s.date}>2021-11-11</div>
-              <div className={s.moneyInfo}>
-                <div className={s.moneyInfoItem}>
-                  <div className={`${s.fIcon} ${s.green}`}>支</div>
-                  <div className={s.bold}>￥100.00</div>
+          {billList.map((item, index) => {
+            return (
+              <div className={s.item} key={index}>
+                <div className={s.itemTop}>
+                  <div className={s.date}>2021-11-11</div>
+                  <div className={s.moneyInfo}>
+                    <div className={s.moneyInfoItem}>
+                      <div className={`${s.fIcon} ${s.green}`}>支</div>
+                      <div className={s.bold}>￥100.00</div>
+                    </div>
+                    <div className={s.moneyInfoItem}>
+                      <div className={`${s.fIcon} ${s.red}`}>收</div>
+                      <div className={s.bold}>￥100.00</div>
+                    </div>
+                  </div>
                 </div>
-                <div className={s.moneyInfoItem}>
-                  <div className={`${s.fIcon} ${s.red}`}>收</div>
-                  <div className={s.bold}>￥100.00</div>
+                <div className={s.payDetailItem}>
+                  <div className={s.productRow}>
+                    <div className={s.goodsName}>购物</div>
+                    <div className={s.payout}>-888.00</div>
+                  </div>
+                  <div className={s.mark}>我可以不用但我不能没有</div>
+                  <div className={s.time}>15:00</div>
                 </div>
               </div>
-            </div>
-            <div className={s.payDetailItem}>
-              <div className={s.productRow}>
-                <div className={s.goodsName}>购物</div>
-                <div className={s.payout}>-888.00</div>
-              </div>
-              <div className={s.mark}>我可以不要但我不能没有</div>
-              <div className={s.time}>15:00</div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </>
